dbd3556db47fba3c3457810fb701f20c55ef4a66
shiiiiiiiii1 <shou1_822@yahoo.co.jp>
2017-06-19 17:49:00 +0900

    finish

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,9 +2,12 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-  ofBackground(0, 0, 0);
+  ofBackground(255, 255, 255);
   ofEnableAlphaBlending();
-  ofSetCircleResolution(64);  
+  ofSetCircleResolution(64);
+  
+  radius = 50.0;
+  alpha = 0;
 }
 
 //--------------------------------------------------------------
@@ -14,60 +17,24 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-  
+  ofSetColor(200, 0, 0, alpha);
+  ofDrawCircle(location_x, location_y, radius);
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){
-  
-}
+  switch (button) {
+    case 0:   // 左
+      location_x = float(x);
+      location_y = float(y);
+      alpha = 150;
+      break;
+    case 1:   // ホイール
+      break;
+    case 2:   // 右
+      if ( sqrtf((location_x-float(mouseX))*(location_x-float(mouseX)) + (location_y-float(mouseY))*(location_y-float(mouseY))) <= radius ) {
+        alpha = 0;
+      }
+      break;
+  }
+};
