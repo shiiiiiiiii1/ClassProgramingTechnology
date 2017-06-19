a94fd5128fdf1756e4da3b5fd3a88c60b214424f
shiiiiiiiii1 <shou1_822@yahoo.co.jp>
2017-06-19 23:51:50 +0900

    temporary finish

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,72 +2,56 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-  ofBackground(0, 0, 0);
+  ofBackground(255, 255, 255);
   ofEnableAlphaBlending();
   ofSetCircleResolution(64);
+  
+  for (int i = 0; i < NUMBALL; i++) {
+    location_x[i] = ofRandom(ofGetWidth());
+    location_y[i] = ofRandom(ofGetHeight());
+    speed_x[i] = ofRandom(-1.5, 1.5);
+    speed_y[i] = ofRandom(-5.0, 5.0);
+    radius[i] = ofRandom(10.0, 30.0);
+  }
+  
+  gravity = 4.0;
+  friction = 0.999;
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-  
+  for (int i = 0; i < NUMBALL; i++) {
+    speed_y[i] = ofRandom(0, 4.0);
+    location_y[i] += (speed_y[i] + gravity);
+    speed_x[i] = ofRandom(-1.5, 1.5);
+    location_x[i] += speed_x[i];
+    if (location_y[i] > ofGetHeight()) {
+      location_x[i] = ofRandom(ofGetWidth());
+      location_y[i] = 0;
+    }
+  }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-  
+  ofSetColor(red, green, blue);
+  for (int i = 0; i < NUMBALL; i++) {
+    ofDrawCircle(location_x[i], location_y[i], radius[i]);
+  }
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){
-  
+  switch (key) {
+    case 49:
+      red = 255;
+      green = 0;
+      blue = 0;
+      break;
+    case 50:
+      red = 200;
+      green = 200;
+      blue = 200;
+      break;
+  }
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -9,14 +9,14 @@ public:
 		void draw();
 		
 		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y);
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+
+  static const int NUMBALL = 100;
+  float location_x[NUMBALL];
+  float location_y[NUMBALL];
+  float speed_x[NUMBALL];
+  float speed_y[NUMBALL];
+  float radius[NUMBALL];
+  int red, green, blue;
+  float gravity;
+  float friction;
 };
