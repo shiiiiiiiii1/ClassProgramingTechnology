213de44a3518bf54b67eaa4a8a04d4e4349d8281
shiiiiiiiii1 <shou1_822@yahoo.co.jp>
2017-06-19 16:34:54 +0900

    update main

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -4,7 +4,7 @@
 void ofApp::setup(){
   ofBackground(0, 0, 0);
   ofEnableAlphaBlending();
-  ofSetCircleResolution(64);
+  ofSetCircleResolution(64);  
 }
 
 //--------------------------------------------------------------
@@ -18,6 +18,56 @@ void ofApp::draw(){
 }
 
 //--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+  
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+  
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y){
+  
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+  
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+  
+}
+
+//--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
+  
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+  
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+  
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+  
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+  
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){
   
 }
