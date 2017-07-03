8df403f322a37638f3ecf0e83d2cfa4fba7cb1d9
shiiiiiiiii1 <shou1_822@yahoo.co.jp>
2017-07-03 16:18:11 +0900

    finish

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,71 +3,53 @@
 //--------------------------------------------------------------
 void ofApp::setup(){
   ofBackground(0, 0, 0);
+  ofEnableSmoothing();
   ofEnableAlphaBlending();
-  ofSetCircleResolution(64);
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE);
+  
+  Image1.load("Sakura1.jpg");
+  Image2.load("Sakura2.jpg");
+  Image3.load("Sakura1.jpg");
+  
+  image_width = (int)Image1.getWidth();
+  image_hight = (int)Image1.getHeight();
+  ofSetWindowShape(image_width * 2, image_hight * 2);
+
+  // r,g,bの順番で値を格納していく
+  Pixels1 = Image1.getPixels();
+  Pixels2 = Image2.getPixels();
+  Pixels3 = Image3.getPixels();
+
+  mixing_ratio = 0.0;
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+  Image3.setFromPixels(Pixels3);
   
+  //  画像Cのビットマップ情報を格納する
+  if (mixing_ratio <= 1.0) {
+    for (int i = 0; i < image_width; i ++) {
+      for (int j = 0; j < image_hight; j ++) {
+        int num = j * 3 * image_width + i * 3;
+        Pixels3[num] = Pixels1[num] * (1 - mixing_ratio) + Pixels2[num] * mixing_ratio;
+        Pixels3[num+1] = Pixels1[num+1] * (1 - mixing_ratio) + Pixels2[num+1] * mixing_ratio;
+        Pixels3[num+2] = Pixels1[num+2] * (1 - mixing_ratio) + Pixels2[num+2] * mixing_ratio;
+      }
+    }
+    mixing_ratio += 0.005;
+  }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-  
+  Image1.draw(0, 0);
+  Image2.draw(0, image_hight);
+  Image3.draw(image_width, 0);
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-  
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){
-  
+  mixing_ratio = 0.0;
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -9,14 +9,14 @@ public:
 		void draw();
 		
 		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y);
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+  
+    ofImage Image1;
+    ofImage Image2;
+    ofImage Image3;
+    ofPixels Pixels1;
+    ofPixels Pixels2;
+    ofPixels Pixels3;
+    int image_width;
+    int image_hight;
+    float mixing_ratio;
 };
